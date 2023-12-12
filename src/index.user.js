6c7bf5c921fa50132d5b3dc120e96da71703cad9
Łukasz Brzózko <lukasz9099@gmail.com>
2023-12-12 20:53:39 +0100

    feat: add fix version link copying

--- src/index.user.js
+++ src/index.user.js
@@ -1,7 +1,79 @@
 (function () {
-  "use strict";
+  const SELECTORS = {
+    linkContainer: "#fixVersions-field",
+    infoMessage: ".my-message-copied-info",
+  };
 
-  const init = () => {};
+  const MAX_ATTEMPTS = 5;
+
+  let attempts = 0;
+
+  const linkStyles = async () => {
+    const myCss = GM_getResourceText("styles");
+    const styleTag = document.createElement("style");
+    styleTag.textContent = myCss;
+
+    document.body.prepend(styleTag);
+  };
+
+  const createClipBoardItem = ({ href, textContent }) => {
+    const clipboardItem = new ClipboardItem({
+      "text/plain": new Blob([textContent], { type: "text/plain" }),
+      "text/html": new Blob([`<a href="${href}">${textContent}</a>`], {
+        type: "text/html",
+      }),
+    });
+
+    return clipboardItem;
+  };
+
+  const generateMessage = () => {
+    const divEl = document.createElement("div");
+    divEl.className = "my-message-copied-info";
+    divEl.textContent = "Fix version link copied to clipboard";
+    document.body.appendChild(divEl);
+  };
+
+  const showMessage = () => {
+    const info = document.querySelector(SELECTORS.infoMessage);
+
+    info.classList.remove("active");
+    void info.offsetWidth; // force reflow
+    info.classList.add("active");
+  };
+
+  const copyLinkIntoClipboard = async (link) => {
+    const clipboardItem = createClipBoardItem(link);
+
+    await navigator.clipboard.write([clipboardItem]);
+
+    showMessage();
+  };
+
+  const handleContextMenu = (e) => {
+    const { clientX, clientY } = e;
+    const linkEl = document.elementFromPoint(clientX, clientY);
+
+    if (!linkEl.search) return;
+
+    e.preventDefault();
+    copyLinkIntoClipboard(linkEl);
+  };
+
+  const init = () => {
+    const linkContainer = document.querySelector(SELECTORS.linkContainer);
+    if (linkContainer) {
+      linkStyles();
+      generateMessage();
+
+      linkContainer.addEventListener("contextmenu", handleContextMenu);
+    } else if (attempts === MAX_ATTEMPTS) {
+      return console.error("Brak kontenera fix version.");
+    } else {
+      attempts++;
+      setTimeout(init, 1000);
+    }
+  };
 
   init();
 })();
